7578ba61a58e41a859974c3663c32041a2286abc
Peter Iskandar <bassingitup@gmail.com>
2016-03-21 21:50:08 -0400

    added http service

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 (function() {
 	var app = angular.module('gemStore', ['gemStore-products']);
 
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
+	app.controller('StoreController', ['$http', function($http){
+		var store = this;
+		store.products = [];
+		$http.get('/products.json').success(function(data){
+			store.products = data;
+		});
+	}]);
 
 	app.controller('ReviewController', function(){
 		this.review = {};
@@ -12,40 +16,4 @@
 			this.review = {};
 		}
 	});
-
-
-	var gems = [
-		{
-			name: 'Dodecahedron',
-			price: 2	,
-			description: 'aksdjaasdkaa',
-			canPurchase: true,
-			images: [
-			{
-				full:"https://www.fillmurray.com/200/300",
-				thumbnail: "https://www.fillmurray.com/100/100"
-			}],
-			reviews: [
-				{
-					stars: 5,
-					body: "great gem!",
-					author: "Jimmy Page"
-				}
-			]
-
-		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: 'aksdjaasdkaa',
-			canPurchase: false,
-			images: [
-			{
-				full:"https://www.fillmurray.com/200/300",
-				thumbnail:	"https://www.fillmurray.com/100/100"
-			}],
-			reviews:[]
-
-		},
-	]
 })();
